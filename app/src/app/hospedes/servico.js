cb122c67d19f4fb73ed0fcf6c12f30287cde0c47
João Manoel Aureliano <jmanoel.web@gmail.com>
2018-12-10 14:28:02 -0200

    Update servico.js

--- app/src/app/hospedes/servico.js
+++ app/src/app/hospedes/servico.js
@@ -4,7 +4,7 @@ export default class HospedeServico extends AbstractCrudService {
 
   constructor($http) {
     super($http, 'https://reserva-facil-betha-sistemas.herokuapp.com/api/hospede')
-    //   super($http, 'https://localhost:8080/api/hospede')
+    //   super($http, 'https://localhost:8080/api/hospedes')
   }
 
 }
